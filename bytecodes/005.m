67dd51985002965b85eba22d2a86275476b6b031
sirnicson <sirnicson.nicson@gmail.com>
2024-10-07 15:58:06 +0300

    Test file for add - some loop errors exit

---
+++ bytecodes/005.m
@@ -0,0 +1,14 @@
+push 5    # 5 onto the stack
+push 10   # 10 onto the stack
+pall      # Print all (10,5)
+
+push 15   # 15 onto the stack (15, 10, 5)
+pop       # Remove top (15)
+
+pint      # Print top (10)
+
+push 20   # 20 onto the stack (20, 10, 5)
+
+add       # Add top two values (20 & 10 = 30)
+
+pall      # Print all (remaining) (20, 10, 5)
